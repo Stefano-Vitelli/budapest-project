9873f64e7cbd5e335311b270ac24797a9822e7a2
Stefano-Vitelli <stefanovitelli9@gmail.com>
2022-10-03 08:22:47 +0200

    cambia texto por algo mas simple y prof

--- src/Navbar.js
+++ src/Navbar.js
@@ -15,18 +15,16 @@ const Navbar = () => {
                          <div className="paragraph">
                                         <h3 className="third-header">About me</h3>
                               <p>
-                                   My name is Stefano Vitelli, a very passionate Trainee Web Developer who always have this two questions in mind into a working team...<br/><br/>
-                                   What make us different? and How can we create an impact?
-
-                                  I have been working with a few technologies like HTML, CSS, JS and REACT to answer these questions and create simple, but powerful websites.
-                                   
+                                   My name is Stefano Vitelli, a very passionate and dedicate Trainee Web Developer.<br/>
+                                   I have been working with a few technologies like HTML, CSS, JS and REACT to create simple, but powerful websites. <br /><br />
+                                   My aim is to find that little thing that make us different and create an impact through web development.
+                              
                               </p>
 
-                                        <h3 className='four-header'>To be more precise...</h3>
+                                        <h3 className='four-header'>Values</h3>
                               <p className='second-paragraph'>  
-                              There are a lot of answers to the same question but I believe what makes you different is the attitude combined with the hard work. Simple?...yes Easy? Of course not...
-
-                              
+                              I believe that being dedicate, hard working, passionate about what you do and always keeping the best attitude into a working team, is the key to face each project and create something really good. <br /><br />
+                              What makes us different? How can we create an impact? Can we create something better? That's the idea...
                               </p>
                               
                               <img src="/Imagenes Web/stars-custom.svg" alt="stars-sky" className="stars-end"/>
