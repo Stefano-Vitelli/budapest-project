986bca94ef5ffadc4afd85ed98505259cea6fb31
Aldo Rojas <aldogrojas5@gmail.com>
2022-10-17 11:02:43 -0300

    final changes

--- src/DinamicNavBar.js
+++ src/DinamicNavBar.js
@@ -1,41 +1,41 @@
 const DinamicNavBar = () => {
   return ( 
-  <header className="navegation">
-    <nav className="nav-style">
-      <div className="personal-logo">
-        <a href="/" aria-label="home">
-          <img
-            src="/web-images/cib-skyliner-white.svg"
-            alt="stars-sky"
-            className="stars"
-          />
-        </a>
-      </div>
-      <div className="information">
-        <ol className="ordered-list">
-          <li className="life-resume">
-            <a
-              href="/cv.pdf"
-              target="_blank"
-              rel="nofollow noopener noreferrer"
-            >
-                Life Resume
-            </a>
-          </li>
-          <li className="ContactMe">
-            <a
-              href="mailto:stefanovitelli9@gmail.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Contact me
-            </a>
-          </li>
-        </ol>
-      </div>
-    </nav>
-  </header>
-);
+    <header className="navegation">
+      <nav className="nav-style">
+        <div className="personal-logo">
+          <a href="/" aria-label="home">
+            <img
+              src="/web-images/cib-skyliner-white.svg"
+              alt="stars-sky"
+              className="stars"
+            />
+          </a>
+        </div>
+        <div className="information">
+          <ol className="ordered-list">
+            <li className="life-resume">
+              <a
+                href="/cv.pdf"
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+              >
+                  Life Resume
+              </a>
+            </li>
+            <li className="ContactMe">
+              <a
+                href="mailto:stefanovitelli9@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Contact me
+              </a>
+            </li>
+          </ol>
+        </div>
+      </nav>
+    </header>
+  );
 }
  
 export default DinamicNavBar;
